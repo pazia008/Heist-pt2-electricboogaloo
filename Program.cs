0e7ad4980a9f2ec31b82d47c75855607482a6f9b
Pazia Ramirez <pazia008@gmail.com>
2021-03-16 11:48:35 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Heisting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
